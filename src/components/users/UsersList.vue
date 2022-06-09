98e127d8cb2f861eea7c0be6fd8c307e6e8d079e
Alex Mwangi <alex@stl-horizon.com>
2022-06-09 22:45:51 +0300

    Programmatic navigation using this..push()

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,4 +1,20 @@
+<!-- Programmatic Navigation - this.$router.push('/path')
+============================
+
+-Let's say for example we have a button in the UsersList component and we wanna use it to navigate to some page
+
+-We have this.$router.push() which we can use inside our export default {} - for programmatic Navigation
+
+-Idearly adding to the webHistory the route you wanna go to
+
+-And then specify which link we want to go to
+  e.g. this.$router.push('/teams') - Redirects to teams page
+        this.$router.back() - Redirects to current page
+        this.$router.forward() - Emulate backward/forward buttons in the browser and much more
+-->
+
 <template>
+<button @click="confirmInput"> Confirm</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -11,6 +27,15 @@ export default {
   components: {
     UserItem,
   },
+
+methods : {
+  confirmInput(){
+    //do something..say navigate to teams page
+
+    this.$router.push('/teams')
+  }
+},
+
   inject: ['users'],
 };
 </script>
