7dc1a7a787c83fe2252d295943861a679bcf8b81
Alex Mwangi <alex@stl-horizon.com>
2022-06-10 15:34:39 +0300

    Added TeamsFooter/ UsersFooter as examples for rendering multiple router-views

---
+++ src/components/users/UsersFooter.vue
@@ -0,0 +1,33 @@
+
+<template>
+<div class="footer">
+    <div class="footer-heading">
+    <h2>Users Footer</h2>
+    </div>
+
+    <div class="footer-copyright">
+        <p>&copy; Coding Pro</p>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+
+.footer {
+    width: 100%;
+    height: 5rem;
+    background-color: #11005c;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h2 {
+    margin-right: 30px;
+    color: #fff;
+}
+p {
+    color: #fff;
+}
+</style>
