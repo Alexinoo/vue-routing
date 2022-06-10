e13481d9b4922b68d8655fec835a326f65e6a0a0
Alex Mwangi <alex@stl-horizon.com>
2022-06-10 12:25:26 +0300

    Added a NotFound component that shows Page not found content - Redirects to this component if url is not found

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>
+        Page not Found ! <router-link to="/teams">Back To teams</router-link>
+    </h2>
+</template>
+
+<style scoped>
+
+h2 {
+    text-align : center;
+}
+</style>
